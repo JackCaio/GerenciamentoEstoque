2912d578b693286d589f743e5b218b6459cb1e9d
JackCaio <caio.iack1@gmail.com>
2023-03-03 02:47:33 -0300

    Arruma dados de busca de usuarios

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -3,7 +3,7 @@ const { User, Group } = require('../models');
 const getUsers = async () => {
   const users = await User.findAll({
     attributes: ['id', 'name'],
-    include: { model: Group, as: 'group' },
+    include: { model: Group, as: 'group', attributes: ['name'] },
   });
   return users;
 };
